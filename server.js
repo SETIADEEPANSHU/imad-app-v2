985382552567fc6ea570b4da83dd2576fa0f25dd
SETIADEEPANSHU <SETIADEEPANSHU@users.noreply.github.com>
2017-02-02 22:02:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/aricle-one',function(req,res){
+    res.send("Article one will be served here");
+})
+
+
+app.get('/aricle-two',function(req,res){
+    res.send("Article two will be served here");
+})
+
+
+app.get('/aricle-three',function(req,res){
+    res.send("Article three will be served here");
+})
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
