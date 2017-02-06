b30dd9e174d19e491f78629a33984a9e8a4cf045
SETIADEEPANSHU <SETIADEEPANSHU@users.noreply.github.com>
2017-02-06 22:33:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,14 @@
 console.log('Loaded!');
 
-var img=document.getElementById("madi");
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+  marginLeft=marginLeft + 5;
+  img.style.marginLeft=marginLeft +'px';
+}
 img.onclick = function()
 {
-    alert("Hey");
+    
+    var interval=setInterval(moveRight,100);
 };
