ac12968d2bda2cc1964d066e10d6ab19b90cd60d
SETIADEEPANSHU <SETIADEEPANSHU@users.noreply.github.com>
2017-02-06 22:39:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,8 +7,7 @@ function moveRight()
   marginLeft=marginLeft + 5;
   img.style.marginLeft=marginLeft +'px';
 }
-img.onclick = function()
-{
+img.onclick = function(){
     
     var interval=setInterval(moveRight,100);
 };
