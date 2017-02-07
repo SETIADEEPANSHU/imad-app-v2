b74662a0ea65603f86c70df026e81d15ae5cfdc9
SETIADEEPANSHU <SETIADEEPANSHU@users.noreply.github.com>
2017-02-07 22:14:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,8 +35,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var counter=0;
 app.get('/ui/counter',function(req,res){
-    res.send(path,join(__dirname,'ui','counter'));
+    counter=counter+1;
+    res.send(counter.toString());
     
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
