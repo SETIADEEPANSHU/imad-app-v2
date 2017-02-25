3551eeaaffa83f081c8e5a8302f9dbe1a7a56ab4
SETIADEEPANSHU <SETIADEEPANSHU@users.noreply.github.com>
2017-02-25 08:25:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,3 +26,18 @@ but.onclick = function(){
  
     
 };
+
+var button=getElementById('submit');
+var names="";
+button.onclick = function()
+{
+  var name=getElementById('name');
+  names=['nam1','name2','name3','name4'];
+  for(var i=0;i<names.length;i++)
+  {
+      nameList+="<li>"+names[i]+"</li>";
+  }
+  var ullist=getElementById('ullist');
+  ullist.innerHTML=nameList;
+  
+};
